142ec2b9b7b533d1e4e7aee45fca79a2e6bb235f
Keithjmm <91273083+GrumpyRobot21@users.noreply.github.com>
2023-09-28 20:44:10 +0100

    added admin.py code

--- backend/pmpal_project/pmpal_app/admin.py
+++ backend/pmpal_project/pmpal_app/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser, Task, Document
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ('username', 'email', 'name', 'is_staff', 'is_active')
+    fieldsets = (
+        (None, {'fields': ('username', 'email', 'password')}),
+        ('Personal info', {'fields': ('name',)}),
+        ('Permissions', {'fields': ('is_staff', 'is_active')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2', 'name', 'is_staff', 'is_active')}
+        ),
+    )
+    search_fields = ('username', 'email', 'name')
+    ordering = ('username',)
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'details', 'completion_date', 'status')
+    list_filter = ('status',)
+    search_fields = ('title', 'details')
+
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = ('task', 'document_file')
+    search_fields = ('task__title',)
+
+# Register the models and the custom admin classes
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(Task, TaskAdmin)
+admin.site.register(Document, DocumentAdmin)

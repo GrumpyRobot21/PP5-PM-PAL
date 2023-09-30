247d737abc7456ad49d31141c0dc722d6e91650f
Keithjmm <91273083+GrumpyRobot21@users.noreply.github.com>
2023-09-30 22:17:54 +0100

    first iteration of login form and additional styling changes on Home page

--- frontend/pmpalr/src/components/HomePage.js
+++ frontend/pmpalr/src/components/HomePage.js
@@ -1,8 +1,14 @@
-import React from 'react';
-import { Link } from 'react-router-dom';  // Import Link from react-router-dom
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import './HomePage.css';
 
 const HomePage = () => {
+  const [isLoginFormVisible, setIsLoginFormVisible] = useState(false);
+
+  const toggleLoginForm = () => {
+    setIsLoginFormVisible((prev) => !prev);
+  };
+
   return (
     <div className="home-page">
       <div className="container title-container">
@@ -11,16 +17,35 @@ const HomePage = () => {
           <Link to="/create-profile" className="buttons create-profile-button">
             Create Profile
           </Link>
-          <button className="login-button">Login</button>
+          <button className="login-button" onClick={toggleLoginForm}>
+            Login
+          </button>
         </div>
       </div>
-      <div className="container title-container"> {/* Use the same class as above */}
+      <div className="container title-container">
         <p className="welcome-text">
-          Welcome to "PM-PAL," the app to meet all of your task management and organisational needs.
-          Please create a profile. Once this is completed, you will be able to login and start fully realising
-          that organised life you've been waiting for!
+          Welcome to "PM-PAL," the app to meet all of your task management and organizational needs.
+          Please create a profile. Once this is completed, you will be able to log in and start fully realizing
+          that organized life you've been waiting for!
         </p>
       </div>
+
+      {/* Conditionally render the login form based on isLoginFormVisible */}
+      {isLoginFormVisible && (
+        <div className="login-form-container">
+          <form className="login-form">
+            <label htmlFor="username">Username:</label>
+            <input type="text" id="username" name="username" />
+
+            <label htmlFor="password">Password:</label>
+            <input type="password" id="password" name="password" />
+
+            <button type="submit" className="submit-button">
+              Submit
+            </button>
+          </form>
+        </div>
+      )}
     </div>
   );
 };

3fb7a11bfcc8b7bfaa11e1131c50bc247e3d7481
Keithjmm <91273083+GrumpyRobot21@users.noreply.github.com>
2023-10-01 17:08:13 +0100

    added TaskEditForm component and related css component for styling

---
+++ frontend/pmpalr/src/components/TaskEditForm.js
@@ -0,0 +1,64 @@
+// TaskEditForm.js
+
+import React, { useState } from 'react';
+import './TaskEditForm.css';  // Import the CSS file for styling
+
+const TaskEditForm = ({ task, onUpdateTask, onCloseEdit }) => {
+  const [editedTask, setEditedTask] = useState(task);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setEditedTask({ ...editedTask, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onUpdateTask(editedTask);
+    onCloseEdit();
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="task-form">
+      <input
+        type="text"
+        name="title"
+        placeholder="Task Title"
+        value={editedTask.title}
+        onChange={handleChange}
+        className="task-input"
+      />
+      <textarea
+        name="description"
+        placeholder="Task Description"
+        value={editedTask.description}
+        onChange={handleChange}
+        className="task-input"
+      />
+      <input
+        type="date"
+        name="dueDate"
+        value={editedTask.dueDate}
+        onChange={handleChange}
+        className="task-input"
+      />
+
+      <div className="status-dropdown">
+        <label htmlFor="status" className="status-label">Edit Status:</label>
+        <select name="status" value={editedTask.status} onChange={handleChange} className="status-select">
+          <option value="Open">Open</option>
+          <option value="In Progress">In Progress</option>
+          <option value="Completed">Completed</option>
+          <option value="Expired">Expired</option>
+        </select>
+      </div>
+
+      {/* Updated styling for the buttons */}
+      <div className="update-cancel-buttons">
+        <button type="submit" className="task-button">Update Task</button>
+        <button onClick={onCloseEdit} className="task-button">Cancel</button>
+      </div>
+    </form>
+  );
+};
+
+export default TaskEditForm;
--- frontend/pmpalr/src/components/TaskPage.js
+++ frontend/pmpalr/src/components/TaskPage.js
@@ -1,11 +1,18 @@
 import React, { useState, useEffect } from 'react';
-import { FaPlus, FaEdit, FaTrash } from 'react-icons/fa';
+import { FaEdit, FaTrash } from 'react-icons/fa';
 import './TaskPage.css';
+import TaskEditForm from './TaskEditForm';
+
+const formatDate = (date) => {
+  const options = { day: 'numeric', month: 'numeric', year: 'numeric' };
+  return new Date(date).toLocaleDateString(undefined, options);
+};
 
 const TaskPage = () => {
   const [tasks, setTasks] = useState([]);
   const [taskData, setTaskData] = useState({ title: '', description: '', dueDate: '', status: 'Open' });
   const [showCreateTaskFields, setShowCreateTaskFields] = useState(false);
+  const [editingTaskId, setEditingTaskId] = useState(null);
 
   // Add a test task on component mount for demonstration
   useEffect(() => {
@@ -19,9 +26,20 @@ const TaskPage = () => {
     setTasks([testTask]);
   }, []);
 
+  const statusOptions = ['Open', 'In Progress', 'Completed', 'Expired'];
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setTaskData({ ...taskData, [name]: value });
+  };
+
+  const handleTaskEdit = (taskId) => {
+    setEditingTaskId(taskId);
+  };
+
+  const handleUpdateTask = (updatedTask) => {
+    setTasks(tasks.map((task) => (task.id === updatedTask.id ? updatedTask : task)));
+    setEditingTaskId(null); // Stop editing after updating
   };
 
   const handleTaskSubmit = (e) => {
@@ -36,40 +54,55 @@ const TaskPage = () => {
     setTasks(tasks.filter((task) => task.id !== taskId));
   };
 
-  const handleTaskStatusUpdate = (taskId, newStatus) => {
-    setTasks(tasks.map((task) => (task.id === taskId ? { ...task, status: newStatus } : task)));
-  };
+  // const handleTaskStatusUpdate = (taskId, newStatus) => {
+  //   setTasks(tasks.map((task) => (task.id === taskId ? { ...task, status: newStatus } : task)));
+  // };
 
-  const toggleDescription = (taskId) => {
-    const descriptionElement = document.getElementById(`task-description-${taskId}`);
-    if (descriptionElement) {
-      descriptionElement.style.display = descriptionElement.style.display === 'none' ? 'block' : 'none';
-    }
-  };
+  // const toggleDescription = (taskId) => {
+  //   const descriptionElement = document.getElementById(`task-description-${taskId}`);
+  //   if (descriptionElement) {
+  //     descriptionElement.style.display = descriptionElement.style.display === 'none' ? 'block' : 'none';
+  //   }
+  // };
 
   return (
     <div className="task-page">
       <h1 className="pm-pal-title styled-title">PM-PAL</h1>
+
+      <div className="top-right-buttons">
+        <button className="edit-profile-button">Edit Profile</button>
+        <button className="logout-button">Logout</button>
+      </div>
 
       <div className="task-container">
         <button
           className="create-task-button"
           onClick={() => setShowCreateTaskFields((prev) => !prev)}
         >
-
           Create Task
         </button>
 
         {showCreateTaskFields && (
           <form onSubmit={handleTaskSubmit} className="task-form">
-            <input type="text" name="title" placeholder="Task Title" value={taskData.title} onChange={handleChange} />
+            <input
+              type="text"
+              name="title"
+              placeholder="Task Title"
+              value={taskData.title}
+              onChange={handleChange}
+            />
             <textarea
               name="description"
               placeholder="Task Description"
               value={taskData.description}
               onChange={handleChange}
             />
-            <input type="date" name="dueDate" value={taskData.dueDate} onChange={handleChange} />
+            <input
+              type="date"
+              name="dueDate"
+              value={taskData.dueDate}
+              onChange={handleChange}
+            />
             <button type="submit">Create Task</button>
           </form>
         )}
@@ -84,10 +117,12 @@ const TaskPage = () => {
 
         {/* Task Search */}
         <div className="task-search">
-          <input type="text" placeholder="Search by title, due date, status" className="search-input" />
-          <button className="search-button">
-            Search Tasks
-          </button>
+          <input
+            type="text"
+            placeholder="Search by title, due date, status"
+            className="search-input"
+          />
+          <button className="search-button">Search Tasks</button>
         </div>
 
         {/* Task List */}
@@ -100,16 +135,24 @@ const TaskPage = () => {
             <div className="task-info">{task.status}</div>
 
             {/* Due Date */}
-            <div className="task-info">{task.dueDate}</div>
+            <div className="task-info">{formatDate(task.dueDate)}</div>
 
             {/* Actions */}
             <div className="task-actions">
-              <FaEdit className="action-icon" />
+              <FaEdit className="action-icon" onClick={() => handleTaskEdit(task.id)} />
               <FaTrash className="action-icon" onClick={() => handleTaskDelete(task.id)} />
             </div>
           </div>
         ))}
 
+        {/* Display TaskEditForm for the selected task being edited */}
+        {editingTaskId && (
+          <TaskEditForm
+            task={tasks.find((task) => task.id === editingTaskId)}
+            onUpdateTask={handleUpdateTask}
+            onCloseEdit={() => setEditingTaskId(null)}
+          />
+        )}
       </div>
     </div>
   );
